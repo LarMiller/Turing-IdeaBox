b008aa59556cdde243924578a20507eda4156ad7
Lee Graham <graham.la3@gmail.com>
2018-02-13 21:08:52 -0700

    Begin implementing JS functionality.

---
+++ script.js
@@ -0,0 +1,48 @@
+var userInputIdeaTitle = document.querySelector('.ideaTitle');
+var userInputIdeaDescription = document.querySelector('.ideaDescription');
+var userSaveBtn = document.querySelector('#saveButton');
+var userSearchField = document.querySelector('.searchField');
+var userIdeaCard = document.querySelector('article');
+var deleteCardBtn = document.querySelector('.deleteBtn');
+var upVoteCardBtn = document.querySelector('.upVoteBtn')
+var downVoteCardBtn = document.querySelector('.downVoteBtn')
+var dropDownSelector = document.querySelector('.qualitySelector')
+var ideaCardDeck = document.querySelector('#listedIdeas')
+
+
+userSaveBtn.addEventListener('click', userSubmit()); 
+        function userSubmit() {
+  if (userInputIdeaTitle.value.length > 0 && userInputIdeaDescription.value.length > 0) {
+    var userIdeaCard = document.createElement('article');
+    userIdeaCard.innerHTML =       
+      `<h2>${userInputIdeaTitle.value}</h2>
+        <form class="secondForms">
+        <button class="deleteBtn">
+        <img class="deleteImg" src="delete.svg">
+      </button>
+        <p>${userInputIdeaDescription.value}</p>
+        <button class="upVoteBtn">
+        <img class="upVoteImg" src="upvote.svg">
+      </button>
+      <button class="downVoteBtn">
+        <img class="downVoteImg" src="downvote.svg">
+      </button>
+          <select class="qualitySelector" name="quality">
+          quality:
+            <option value="swill">
+              swill
+            </option>
+            <option value="plausible">plausible
+            </option>
+            <option value="genius">genius
+            </option>
+          </select>
+        </form>`
+
+    ideaCardDeck.appendChild(userIdeaCard);
+    // userSaveBtn.disabled = true;
+    // // clearInputFields();
+    // countLinks();
+    }
+}
+
