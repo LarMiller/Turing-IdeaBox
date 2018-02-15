286794dc385be82b5ea32c9eefe19e22b63c4a94
Lee Graham <graham.la3@gmail.com>
2018-02-15 12:48:53 -0700

    Implement button hover images and aria tags for accessibility.

--- script.js
+++ script.js
@@ -10,53 +10,55 @@ var ideaCardDeck = document.querySelector('#listedIdeas');
 
 userSaveBtn.addEventListener('click', userSubmit);
 ideaCardDeck.addEventListener('click', deleteIdeaCard);
-// userSaveBtn.addEventListener('click', getTime);
 
 function userSubmit() {
   if (userInputIdeaTitle.value.length > 0 && userInputIdeaDescription.value.length > 0) {
     var userIdeaCard = document.createElement('article');
     var uniqueId = Math.round(new Date().getTime(userSaveBtn)/1000);
-    }
-    userIdeaCard.innerHTML = `<h2 id="${uniqueId}">${userInputIdeaTitle.value}</h2>
-        <button class="deleteBtn">
-        <img class="deleteImg" src="delete.svg">
+  }
+    userIdeaCard.innerHTML = 
+      `<article id="${uniqueId}" aria-live="assertive">
+      <h2>${userInputIdeaTitle.value}</h2>
+      <button class="deleteBtn" alt="buttonToDeleteIdea">
       </button>
-        <p>${userInputIdeaDescription.value}</p>
-        <button class="upVoteBtn">
-        <img class="upVoteImg" src="upvote.svg">
+      <p>${userInputIdeaDescription.value}</p>
+      <button class="upVoteBtn" alt="buttonToVoteIdeaUp">
       </button>
-      <button class="downVoteBtn">
-        <img class="downVoteImg" src="downvote.svg">
+      <button class="downVoteBtn" alt="buttonToVoteIdeaDown">
       </button>
-          <select class="qualitySelector" name="quality">
-          quality:
-            <option value="swill">
-              swill
-            </option>
-            <option value="plausible">plausible
-            </option>
-            <option value="genius">genius
-            </option>
-          </select>`;
-
+      <select class="qualitySelector" name="quality">quality:
+        <option value="swill">swill
+        </option>
+        <option value="plausible">plausible
+        </option>
+        <option value="genius">genius
+        </option>
+      </select>
+      </article>`;
     ideaCardDeck.prepend(userIdeaCard);
+    storeMyJson()
     clearInputFields();
-    // countLinks();
-    }
+}
 
+function storeMyJson() {
+    var jsonStore = {"id":"uniqueId", "h2":
+    "userInputIdeaTitle.value",
+      "p":"userInputIdeaDescription.value"}
+      console.log('hahaaaaa')
+      var refresh = JSON.stringify(jsonStore);
+      localStorage.setItem(jsonStore.id, refresh);
+}
 
-
-function clearInputFields(){
+function clearInputFields() {
   userInputIdeaTitle.value = "";
   userInputIdeaDescription.value = "";
 }
 
 function deleteIdeaCard(event) {
-  // debugger;
   var foo = document.querySelector('article').children[1];
   if(event.target === foo) {
     console.log('heeeeyyy');
     foo.parentNode.remove();
-    // event.target.parentNode.parentNode.remove();
+    // localStorage,removeItem('id');
   }
 }
